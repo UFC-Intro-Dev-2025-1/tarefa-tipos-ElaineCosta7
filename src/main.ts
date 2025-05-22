951ffd86a5a359111a16bf70d7242153baf8b5dc
ElaineCosta7 <elainetiny616@gmail.com>
2025-05-22 20:57:51 +0000

    conclusão de atividade

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-// Escreva seu código aqui!
+let numeroDeCadeiras: number;
+
+numeroDeCadeiras = 21;
+
+console.log(numeroDeCadeiras);
+
+let professora: string;
+
+professora = 'Lana';
+
+console.log(professora);
+
+let hojeTemAula: boolean;
+
+hojeTemAula = true;
+
+console.log(hojeTemAula);
+
+let corDaParede: string;
+
+console.log(corDaParede);
+
+interface Aluno {
+    nome: string;
+    nota: number;
+}
+
+let alunoDestaque: Aluno = {
+    nome: 'Tayna',
+    nota: 9,
+};
+
+console.log(alunoDestaque.nome + '\nnota: ' + alunoDestaque.nota);
